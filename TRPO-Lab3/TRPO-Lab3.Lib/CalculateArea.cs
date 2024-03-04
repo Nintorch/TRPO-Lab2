ed0c4d1411cab0541ddd23f6258713b410e202fc
Nintorch <testovprivet@gmail.com>
2024-03-04 11:21:00 +0500

    Functionality fix

--- TRPO-Lab3/TRPO-Lab3.Lib/CalculateArea.cs
+++ TRPO-Lab3/TRPO-Lab3.Lib/CalculateArea.cs
@@ -6,6 +6,11 @@
 
         public static double CalculateConeArea(double radius, double height)
         {
+            if (radius < 0 || height < 0)
+            {
+                throw new ArgumentException("Radius or height is negative");
+            }
+
             double l = Math.Sqrt(radius*radius + height*height);
             return Math.PI * radius * (l + radius);
         }
