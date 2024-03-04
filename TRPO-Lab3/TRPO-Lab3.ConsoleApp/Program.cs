9eaf3e1d185bc5889ecbcc62555e65154f9108a1
Nintorch <testovprivet@gmail.com>
2024-03-04 10:53:24 +0500

    First commit

---
+++ TRPO-Lab3/TRPO-Lab3.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TRPO_Lab3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
