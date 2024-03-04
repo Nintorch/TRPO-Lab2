f5733c768c09ff997a4fc84f0fee0ea3fcfd8dff
Nintorch <testovprivet@gmail.com>
2024-03-04 11:28:10 +0500

    Added new unit tests

--- TRPO-Lab3/TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3/TRPO-Lab3.Tests/UnitTest1.cs
@@ -17,5 +17,22 @@ namespace TRPO_Lab3.Tests
             Assert.Greater(area, expected - 0.001);
             Assert.Greater(expected + 0.001, area);
         }
+
+        [Test]
+        public void Test2()
+        {
+            double area = CalculateArea.CalculateConeArea(20, 10);
+            double expected = 2661.6;
+            Assert.Greater(area, expected - 0.001);
+            Assert.Greater(expected + 0.001, area);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Throws(typeof(ArgumentException) ,() => { CalculateArea.CalculateConeArea(-10, 10); });
+            Assert.Throws(typeof(ArgumentException), () => { CalculateArea.CalculateConeArea(10, -10); });
+            Assert.Throws(typeof(ArgumentException), () => { CalculateArea.CalculateConeArea(-10, -10); });
+        }
     }
 }
