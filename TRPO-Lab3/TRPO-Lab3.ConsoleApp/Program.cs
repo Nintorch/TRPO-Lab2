78f20fb3b43864c3f6ab3228fe5833750b77bb96
Nintorch <testovprivet@gmail.com>
2024-03-07 12:20:27 +0500

    Console app

--- TRPO-Lab3/TRPO-Lab3.ConsoleApp/Program.cs
+++ TRPO-Lab3/TRPO-Lab3.ConsoleApp/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace TRPO_Lab3.ConsoleApp
+﻿using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Расчёт площади поверхности круглого конуса");
+
+            Console.WriteLine("Введите радиус конуса:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите высоту конуса:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("Результат: " + CalculateArea.CalculateConeArea(radius, height));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
