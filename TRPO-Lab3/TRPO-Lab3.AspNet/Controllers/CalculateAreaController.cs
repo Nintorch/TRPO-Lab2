248499ad61c2bd91c9409df956f0e1896fb46e38
Nintorch <testovprivet@gmail.com>
2024-03-16 10:46:21 +0500

    First attempt at writing a controller

---
+++ TRPO-Lab3/TRPO-Lab3.AspNet/Controllers/CalculateAreaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRPO_Lab3.AspNet.Models;
+
+namespace TRPO_Lab3.AspNet.Controllers
+{
+    public class CalculateAreaController : Controller
+    {
+        private double radius = 0, height = 0;
+
+        // GET: CalculateAreaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(new CalculateAreaViewModel(radius, height));
+        }
+
+        // POST: CalculateAreaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                collection.TryGetValue(nameof(CalculateAreaViewModel.Radius), out var new_radius);
+                collection.TryGetValue(nameof(CalculateAreaViewModel.Height), out var new_height);
+                radius = Convert.ToDouble(new_radius);
+                height = Convert.ToDouble(new_height);
+                return RedirectToAction(nameof(Edit));
+            }
+            catch
+            {
+                return View(new CalculateAreaViewModel(radius, height));
+            }
+        }
+    }
+}
