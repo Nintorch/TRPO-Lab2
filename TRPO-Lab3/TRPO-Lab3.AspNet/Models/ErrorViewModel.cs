80c59de657b95f8e1bd39c7ba3119df735fff029
Nintorch <testovprivet@gmail.com>
2024-03-16 10:43:20 +0500

    new AspNet project

---
+++ TRPO-Lab3/TRPO-Lab3.AspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TRPO_Lab3.AspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
