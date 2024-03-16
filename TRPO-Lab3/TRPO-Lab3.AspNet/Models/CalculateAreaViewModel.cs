cf58e83183f6310db05e3355a941d5f22b7b596e
Nintorch <testovprivet@gmail.com>
2024-03-16 10:43:47 +0500

    New CalculateArea View Model

---
+++ TRPO-Lab3/TRPO-Lab3.AspNet/Models/CalculateAreaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.AspNet.Models
+{
+    public class CalculateAreaViewModel
+    {
+        public double Radius { get; set; }
+
+        public double Height { get; set; }
+
+        public double Area
+        {
+            get => CalculateArea.CalculateConeArea(Radius, Height);
+        }
+
+        public CalculateAreaViewModel(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+    }
+}
